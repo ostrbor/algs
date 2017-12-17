49cad3425ff2ac53c600be2224dfc54bb6063b93
boris <ostrbor@gmail.com>
2017-12-17 20:39:21 +0300

    Complete fast

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,15 +1,48 @@
 import java.lang.IllegalArgumentException;
+import java.util.Arrays;
 
 public class FastCollinearPoints {
-    public FastCollinearPoints(Point[] points) {
+    private Point[] pointsArray;
+    private int numberOfPointsInLine = 4;
+    private int lineSegmentIndex = 0;
 
+    public FastCollinearPoints(Point[] points) {
+        for (int k = 0; k < points.length; k++) {
+            if (points[k] == null) throw new IllegalArgumentException("null point is not allowed");
+        }
+        for (int i = 0; i < points.length - 1; i++) {
+            Point p = points[i];
+            for (int j = i + 1; j < points.length; j++) {
+                Point q = points[j];
+                if (p.compareTo(q) == 0) throw new IllegalArgumentException("same points are not allowed");
+            }
+        }
+        pointsArray = points;
     }
 
     public int numberOfSegments() {
-
+        return lineSegmentIndex + 1;
     }
 
     public LineSegment[] segments() {
+        LineSegment[] segs = new LineSegment[pointsArray.length];
 
+        for (int i = 0; i <= (pointsArray.length - numberOfPointsInLine); i++) {
+            Point p = pointsArray[i];
+            Arrays.sort(pointsArray, p.slopeOrder());
+            for (int j = i + 1; j < pointsArray.length; j++) {
+                Point second = pointsArray[j];
+                Point third = pointsArray[j + 1];
+                Point fourth = pointsArray[j + 2];
+                if ((p.slopeTo(second) == p.slopeTo(third)) && (p.slopeTo(second) == p.slopeTo(fourth))) {
+                    LineSegment ls = new LineSegment(p, fourth);
+                    segs[lineSegmentIndex++] = ls;
+                }
+            }
+        }
+
+        LineSegment[] res = new LineSegment[lineSegmentIndex + 1];
+        for (int i = 0; i < res.length; i++) res[i] = segs[i];
+        return res;
     }
 }
