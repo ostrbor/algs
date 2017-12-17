49add12b20cb282c6a940274278bcee8df79dc80
boris <ostrbor@gmail.com>
2017-12-17 14:06:24 +0300

    Add constructor for brute force

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,8 +1,20 @@
 import java.lang.IllegalArgumentException;
 
 public class BruteCollinearPoints {
-    public BruteCollinearPoints(Point[] points) {
+    private Point[] points;
 
+    public BruteCollinearPoints(Point[] points) {
+        for (int k = 0; k < points.length; k++) {
+            if (points[k] == null) throw new IllegalArgumentException("null point is not allowed");
+        }
+        for (int i = 0; i < points.length - 1; i++) {
+            Point p = points[i];
+            for (int j = i + 1; j < points.length; j++) {
+                Point q = points[j];
+                if (p.compareTo(q) == 0) throw new IllegalArgumentException("same points are not allowed");
+            }
+        }
+        points = points;
     }
 
     public int numberOfSegments() {
