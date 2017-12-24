a948e08b08edd52e09e2f2eb2d5ac9f348e6ef48
boris <ostrbor@gmail.com>
2017-12-24 22:09:18 +0300

    Finish manhattan

--- src/Board.java
+++ src/Board.java
@@ -31,9 +31,15 @@ public class Board {
         int dim = dimension();
         for (int i = 0; i < dim; i++) {
             for (int j = 0; j < dim; j++) {
-                int expected = (i + 1) * (j + 1);
-                if (blocks[i][j] != expected) {
-                    distanceCurrent = (int) Math.ceil(expected / (double) dim);
+                int actual = blocks[i][j];
+                if (actual == 0) continue;  // don't count 0
+                int expected = (i * dim) + (j + 1);
+                if (actual != expected) {
+                    int rowIndex = actual / dim;
+                    int colIndex = actual % dim - 1;
+                    int rowDistance = Math.abs((i - rowIndex));
+                    int colDistance = Math.abs((j - colIndex));
+                    distanceCurrent = rowDistance + colDistance;
                     distanceTotal += distanceCurrent;
                 }
             }
@@ -72,33 +78,51 @@ public class Board {
     }
 
     public static void main(String[] args) {
-        int[][] b1 = {{1, 3}, {2, 0}};
+        int[][] b1 = {
+                {1, 3},
+                {2, 0}};
         Board bd1 = new Board(b1);
         assert 2 == bd1.hamming();
 
-        int[][] b2 = {{0, 2}, {3, 1}};
+        int[][] b2 = {
+                {0, 2},
+                {3, 1}};
         Board bd2 = new Board(b2);
         assert 1 == bd2.hamming();
 
-        int[][] a = {{1, 2}, {3, 0}};
+        int[][] a = {
+                {1, 2},
+                {3, 0}};
         Board board = new Board(a);
         assert board.isGoal();
         System.out.print(board);
 
 
-        int[][] c = {{4, 2, 3}, {1, 5, 6}, {7, 8, 0}};
+        int[][] c = {
+                {4, 2, 3},
+                {1, 5, 6},
+                {7, 8, 0}};
         Board board2 = new Board(c);
         assert 2 == board2.manhattan();
 
-        int[][] c1 = {{7, 2, 3}, {4, 5, 6}, {1, 8, 0}};
+        int[][] c1 = {
+                {7, 2, 3},
+                {4, 5, 6},
+                {1, 8, 0}};
         Board board3 = new Board(c1);
         assert 4 == board3.manhattan();
 
-        int[][] c2 = {{6, 2, 3}, {4, 5, 1}, {7, 8, 0}};
+        int[][] c2 = {
+                {6, 2, 3},
+                {4, 5, 1},
+                {7, 8, 0}};
         Board board4 = new Board(c2);
         assert 6 == board4.manhattan();
 
-        int[][] c3 = {{0, 2, 3}, {4, 5, 6}, {7, 8, 1}};
+        int[][] c3 = {
+                {0, 2, 3},
+                {4, 5, 6},
+                {7, 8, 1}};
         Board board5 = new Board(c3);
         assert 4 == board5.manhattan();
     }
